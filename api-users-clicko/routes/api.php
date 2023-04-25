199a247ff8ab59e47009a7378193f92beab1faa3
marctl01 <mtorrubia42@gmail.com>
2023-04-25 20:52:59 +0200

    Feat- Domain endpoint

--- api-users-clicko/routes/api.php
+++ api-users-clicko/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DomainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,8 @@ Route::post('/users', [UserController::class, 'store']);
 Route::get('/users/{id}', [UserController::class, 'show']);
 Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+Route::get('/domains', [DomainController::class, 'index']);
 
 
 
